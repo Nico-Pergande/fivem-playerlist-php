ebe7bfe18a58cb32cc66f671e0e895a6127c40b8
Nico Pergande <72522450+Nico-Pergande@users.noreply.github.com>
2022-03-10 00:38:06 +0100

    new!

--- index.php
+++ index.php
@@ -1,32 +1,56 @@
-<meta name="description" content="LoxRP - Spielerliste">
-<meta name="author" content="Nico Pergande">
-<link rel="shortcut icon" href="server.png" />
-<style> @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap'); </style>
-<link rel="stylesheet" href="./style.css">
+<?php
+    // $players = file_get_contents('http://141.95.72.179:30120/players.json');
+    $players = file_get_contents('http://ic.npdev.eu:30120/players.json');
+    $infos = file_get_contents('http://ic.npdev.eu:30120/info.json');
+    $players = json_decode($players, true);
+    $infos = json_decode($infos, true);
+?>
 
+<title><?php echo $infos['vars']['sv_projectName']; ?> - <?php echo count($players); ?> Spieler</title>
+<meta name="description" content="Spielerldaten des Servers <?php echo $infos['vars']['sv_projectName']; ?> mit <?php echo count($infos['resources']); ?> Resourcen.">
+<meta name="author" content="nico-pergande.dev">
 
-<section id="head">
+<div class="playerlist">
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>SteamId</th>
+            <th>DiscordId</th>
+            <th>FiveM License</th>
+            <th>Ping</th>
+        </tr>
+        <?php foreach($players as $player) { ?>
+            <tr>
+                <td><?php echo $player['id'] ?></td>
 
-</section>
+                <td><?php echo $player['name'] ?></td>
 
+                <?php
+                foreach($player['identifiers'] as $identifier) {
+                    if (str_starts_with($identifier, 'steam:')) {
+                        ?> <td><?php echo $identifier ?></td> <?php
+                    };
+                }
+                ?>
 
-<br>
-<br>
-<br>
+                <?php
+                    foreach($player['identifiers'] as $identifier) {
+                        if (str_starts_with($identifier, 'discord:')) {
+                            ?> <td><?php echo $identifier ?></td> <?php
+                        };
+                    }
+                ?>
 
-<section id="list">
-
-</section>
-
-<script>
-    setInterval(reload, 3000)
-
-    function reload() {
-        $('#head').load('head.php')fadeIn("Slow");
-        $('#list').load('playerlist.php')fadeIn("Slow");
-    }
-</script>
-
-<hr>
-<p class="bottom">powerd by <a class="bottom" href="https://nico-pergande.dev">nico-pergande.dev</a></p>
-<hr>
+                <?php
+                foreach($player['identifiers'] as $identifier) {
+                    if (str_starts_with($identifier, 'license:')) {
+                        ?> <td><?php echo $identifier ?></td> <?php
+                    };
+                }
+                ?>
+                <td><?php echo $player['ping'] ?></td>
+            </tr>
+        <?php }; ?>
+    </table>
+</div>
